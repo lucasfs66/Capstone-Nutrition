8100d1f72455116c787ecdbdb7744e1a2a87a6ed
Lucas <lucasfreiresabino@gmail.com>
2022-11-11 16:32:46 -0700

    image get is working

--- public/index.js
+++ public/index.js
@@ -6,24 +6,32 @@ axios.get('/recipe')
     .then((res) => {
         let objArr = res.data
         console.log(res.data)
-        for(let i = 0; i <objArr.length; i++){
-            getRecipes(i, objArr)
+        let count = 1
+        for(let i = 0; i < objArr.length; i++){
+           getRecipes(objArr, i) 
+        }    
+        
             
-            imagePlace.appendChild(imageDiv)
-        }
+            
     })
     .catch(err => console.log(err))
 
 
 
-const getRecipes = (i, objArr) => {
+const getRecipes = (obj, x, count) => {
+    
     let imageDiv = document.createElement('div')
     imageDiv.classList.add('image-page')
-
-    imageDiv.innerHTML = `
-            <img src="${objArr[i].imgurl}">
-            <h3>${objArr[i].recipename}</h3>
-    `
-
     
+    imageDiv.innerHTML = `
+            <img src="${obj[x].imgurl}" class="image">
+            <h3>${obj[x].recipename}</h3>
+    `
+    if(x % 2 !== 0) {
+        imageDiv.classList.add('right')
+    } else {
+        imageDiv.classList.add('left')
+
+    }
+    imagePlace.appendChild(imageDiv)
 }
