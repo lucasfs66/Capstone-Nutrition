a04be5d6adb3f5a0c47528ac7b3f4ae4744e38cd
Lucas <lucasfreiresabino@gmail.com>
2022-11-15 16:06:59 -0700

    finish the calculater

---
+++ public/calculater.js
@@ -0,0 +1,70 @@
+const form = document.querySelector('form')
+const footer = document.querySelector('footer')
+
+const button = document.querySelector('button')
+
+const write = result =>{
+    
+    
+    footer.innerHTML = `
+    <div class="footer">
+            <section class="maintain">
+                <h2>${Math.floor(result)} lb per day</h2>
+                <p>maintain your weight</p>
+            </section>
+            <section class="loseOne">
+                <h2>${Math.floor(result) - 500} lb per day</h2>
+                <p>lose 1 pound per week</p>
+            </section>
+            <section class="loseTwo">
+                <h2>${Math.floor(result) - 1000} lb per day</h2>
+                <p>lose 2 pound per week</p>
+            </section>
+    </div>
+    `
+}
+
+const post = (event) => {
+    event.preventDefault()
+    const ageYou = document.getElementById('age')
+    const male = document.getElementById('male')
+    const female = document.getElementById('female')
+    const cmYou = document.getElementById('height-cm')
+    const weightYou = document.getElementById('weight')
+
+    let object = {
+        age: Number(ageYou.value),
+        height: Number(cmYou.value),
+        weight: Number(weightYou.value)
+    }
+
+    if(male.checked){
+        object['gender'] = false
+    }else if(female.checked){
+        object['gender'] = true
+    }
+
+    
+    if(object.age === '' || object.height === '' || object.weight === '' || object.gender === ''){
+        alert('Fill out everything')
+        return object
+    }
+    console.log(object)
+    
+    axios.post('/calculate',object)
+        .then((res) => { 
+
+            write(res.data.result) 
+            
+        })
+        .catch(err => {
+            console.log(err)
+            console.log('Hello')
+        })
+    alert('recipe created')
+
+}
+
+
+
+button.addEventListener('click', post)
--- server/server.js
+++ server/server.js
@@ -17,7 +17,19 @@ app.get('/recipe', getRecipe);
 
 app.post('/specrecipe', getSpecRecipe);
 
-app.post('/createrecipe', createRecipe)
+app.post('/createrecipe', createRecipe);
+
+app.post('/calculate', (req, res) =>{
+        let {age, height, weight, gender} = req.body
+        let result = 0
+        if(gender === false){
+            result = 88.362 + (13.397 * (weight / 2.20462)) + (4.799 * height) - (5.677 * age) 
+        }else if(gender === true){
+            result = 447.593 + (9.247 * (weight / 2.20462)) + (3.098 * height) - (4.330 * age)
+        }
+            console.log(result)
+            res.status(200).send({result: result})
+});
 
 app.use(express.static('public'));
 
